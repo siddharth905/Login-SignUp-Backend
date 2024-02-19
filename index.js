437d25f61f78619dc24801c7c9faaa294fd16fe4
siddharth905 <siddharthkalamkar905@gmail.com>
2024-02-19 19:46:28 +0530

    Mongo DB Database connected using Atlas

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ const app=express();
 
 require("dotenv").config();
 const PORT=process.env.PORT || 4000
+
+//MiddleWare
+app.use(express.json());
+
+require("./config/database").connect();
+
+app.listen(PORT,()=>{
+    console.log(`App is listening at ${PORT}`);
+})
